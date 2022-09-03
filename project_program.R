94d6f0ae29fec3acf77ac01ee27725ab4835cd09
ooleole0 <oo4766@gmail.com>
2022-09-03 22:26:31 +0800

    adjust date filter to the later lines

--- project_program.R
+++ project_program.R
@@ -1,19 +1,20 @@
 library(tidyverse)
 library(zoo)
 
-# read the project data as a tibble.
-raw_data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
+# read the project data as a tibble
+data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
   mutate(
     # transform date variable into date-datatype
     date = as.Date(as.character(date), "%Y%m%d"),
     # valuate absolute value of prices since negative prices mean the stock was
-    # not trade on the last day of the month.
+    # not traded on the last day of the month
     BIDLO = abs(BIDLO),
     ASKHI = abs(ASKHI),
     PRC = abs(PRC)) %>%
+  # group by PERMNO then calculate last 12 months rolling avg trading volumes
+  group_by(PERMNO) %>%
+  mutate(roll_vol = rollmean(VOL, k =12, na.pad=TRUE, align="right")) %>%
   # filter share codes to 10 and 11, and data date after 2000 only
   filter(SHRCD %in% c(10, 11),
-         date >= '2000-01-01') %>%
-  # group by PERMNO then calculate last 12 months rolling avg trading volumes.
-  group_by(PERMNO) %>%
-  mutate(roll_vol = rollmean(VOL, k =12, na.pad=TRUE, align="right"))
+         date >= '2000-01-01') 
+  
