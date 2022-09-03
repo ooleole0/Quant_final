6475a3f26dd82aba6538e79d90aeddc80d014da0
ooleole0 <oo4766@gmail.com>
2022-09-03 19:42:27 +0800

    1. save memory 2. calculate last 12 months rolling avg trading volumes

--- project_program.R
+++ project_program.R
@@ -1,14 +1,18 @@
 library(tidyverse)
 library(zoo)
 
-# read the project data as tibble.
-raw_data <- as_tibble(read.csv("project_data.csv"))
-
-# transform date variable to date-datatype, and find absolute value of prices.
-data_date <- raw_data %>%
+# read the project data as a tibble.
+raw_data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
   mutate(
+    # transform date variable into date-datatype
     date = as.Date(as.character(date), "%Y%m%d"),
+    # valuate absolute value of prices since negative prices mean the stock was
+    # not trade on the last day of the month.
     BIDLO = abs(BIDLO),
     ASKHI = abs(ASKHI),
     PRC = abs(PRC)) %>%
-  filter(SHRCD %in% c(10, 11))
+  # filter share codes to 10 and 11 only.
+  filter(SHRCD %in% c(10, 11)) %>%
+  # group by PERMNO then calculate last 12 months rolling avg trading volumes.
+  group_by(PERMNO) %>%
+  mutate(roll_vol = rollmean(VOL, k =12, na.pad=TRUE, align="right"))
