c3c646c02b7112f1aa044d81d37e7bdeee5ec080
ooleole0 <oo4766@gmail.com>
2022-09-03 16:54:53 +0800

    1st time data tidying.

--- project_program.R
+++ project_program.R
@@ -1,4 +1,14 @@
-library("tidyverse")
+library(tidyverse)
+library(zoo)
 
-raw_data <- read.csv("project_data.csv")
-raw_data <- as_tibble(raw_data)
+# read the project data as tibble.
+raw_data <- as_tibble(read.csv("project_data.csv"))
+
+# transform date variable to date-datatype, and find absolute value of prices.
+data_date <- raw_data %>%
+  mutate(
+    date = as.Date(as.character(date), "%Y%m%d"),
+    BIDLO = abs(BIDLO),
+    ASKHI = abs(ASKHI),
+    PRC = abs(PRC)) %>%
+  filter(SHRCD %in% c(10, 11))
