a01a9e6e615837a28d74f9a0bf93c4a1bff5f907
ooleole0 <oo4766@gmail.com>
2022-09-04 08:41:48 +0800

    Calculate Amihud ILLIQ

--- project_program.R
+++ project_program.R
@@ -10,10 +10,15 @@ data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
     # not traded on the last day of the month
     BIDLO = abs(BIDLO),
     ASKHI = abs(ASKHI),
-    PRC = abs(PRC)) %>%
+    PRC = abs(PRC),
+    # Amihud ILLIQ
+    VOLD = VOL * PRC,
+    ILLIQ = abs(RET) / VOLD
+    ) %>%
   # group by PERMNO then calculate last 12 months rolling avg trading volumes
   group_by(PERMNO) %>%
   mutate(roll_vol = rollmean(VOL, k =12, na.pad=TRUE, align="right")) %>%
+  ungroup() %>%
   # filter share codes to 10 and 11, and data date after 2000 only
   filter(SHRCD %in% c(10, 11),
          date >= '2000-01-01') 
