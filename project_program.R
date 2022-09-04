cb83896523ed7647c86246417b269ffc51e68bd5
ooleole0 <oo4766@gmail.com>
2022-09-04 17:31:44 +0800

    merge fama-related data

--- project_program.R
+++ project_program.R
@@ -20,6 +20,23 @@ data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
   mutate(roll_vol = rollmean(VOL, k =12, na.pad=TRUE, align="right")) %>%
   ungroup() %>%
   # filter share codes to 10 and 11, and data date after 2000 only
-  filter(SHRCD %in% c(10, 11),
-         date >= '2000-01-01') 
+  filter(SHRCD %in% c(10, 11) & date >= '2000-01-01') %>%
+  mutate(
+    year = year(date),
+    month = month(date)
+  )
+
+# select the needed variables and filter month to June only
+data <- data %>%
+  filter(month == 6) %>%
+  select(year, PERMNO, PRC, RET, VOL, VOLD, ILLIQ, roll_vol) %>%
+  drop_na()
+  
+# read vincent's fama-related data
+data_fama <- read_csv("chars_data") %>%
+  select(PERMNO, gvkey, sorting_year, BM, size)
+
+# merge liquidity and fama-related variables
+data_merged <- data %>%
+  inner_join(data_fama, by = c("PERMNO", "year" = "sorting_year"))
   
