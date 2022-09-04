aab3ec9f8c652382a20652aa6fc40bdd2a9ba453
ooleole0 <oo4766@gmail.com>
2022-09-04 17:31:44 +0800

    merge fama-related data

--- project_program.R
+++ project_program.R
@@ -1,13 +1,14 @@
 library(tidyverse)
 library(zoo)
+library(lubridate)
 
 # read the project data as a tibble
 data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
   mutate(
     # transform date variable into date-datatype
     date = as.Date(as.character(date), "%Y%m%d"),
-    # valuate absolute value of prices since negative prices mean the stock was
-    # not traded on the last day of the month
+    # valuate absolute value of prices since negative prices mean that
+    # the stock was not traded on the last day of the month
     BIDLO = abs(BIDLO),
     ASKHI = abs(ASKHI),
     PRC = abs(PRC),
@@ -19,7 +20,98 @@ data <- read_csv("project_data.csv", na=c("","A","B","C")) %>%
   group_by(PERMNO) %>%
   mutate(roll_vol = rollmean(VOL, k =12, na.pad=TRUE, align="right")) %>%
   ungroup() %>%
-  # filter share codes to 10 and 11, and data date after 2000 only
-  filter(SHRCD %in% c(10, 11),
-         date >= '2000-01-01') 
+  # filter share codes to 10 and 11, and data date after 1999 only
+  filter(SHRCD %in% c(10, 11) & date >= '2000-01-01') %>%
+  mutate(
+    year = year(date),
+    month = month(date)
+  )
+
+# select the needed variables
+data <- data %>%
+  mutate(sorting_year = case_when(
+    month <= 6 ~ year - 1,
+    month >= 7 ~ year)
+           ) %>%
+  select(year, month,sorting_year, PERMNO, PRC, RET, VOL, VOLD, ILLIQ, roll_vol) %>%
+  drop_na()
   
+# read vincent's fama-related data
+data_fama <- read_csv("chars_data") %>%
+  select(PERMNO, gvkey, sorting_year, BM, size)
+
+# merge liquidity and fama-related variables
+data_merged <- data %>%
+  inner_join(data_fama, by = c("PERMNO","sorting_year"))
+
+# remove unnecessary data to save memories
+rm(data, data_fama)
+
+# compute the breakpoints of BM
+BM_breakpoints <- data_merged %>%
+  group_by(year) %>%
+  summarise(
+    BM_q30 = quantile(BM, 0.3, na.rm = TRUE),
+    BM_q70 = quantile(BM, 0.7, na.rm = TRUE)
+  )
+data_merged <- data_merged %>%
+  inner_join(BM_breakpoints, by = "year")
+
+# compute the breakpoints of size
+size_breakpoints <- data_merged %>% 
+  group_by(year) %>%
+  summarise(
+    size_median = quantile(size, 0.5, na.rm = TRUE),
+  )
+data_merged <- data_merged %>%
+  inner_join(size_breakpoints,by = "year")
+
+# flag BM and size
+data_merged <- data_merged %>%
+  mutate(BM_type = case_when(BM <= BM_q30 ~ 'L',
+                             BM > BM_q30 & BM < BM_q70 ~ 'N',
+                             BM >= BM_q70 ~ 'H'),
+         Size_type = case_when(size>= size_median ~ 'B',
+                               size< size_median ~ 'S')
+  )
+
+# compute the breakpoints of rolling volume
+roll_vol_breakpoints <- data_merged %>%
+  group_by(year) %>%
+  summarise(
+    roll_vol_q20 = quantile(roll_vol, 0.2, na.rm = TRUE),
+    roll_vol_q80 = quantile(roll_vol, 0.8, na.rm = TRUE)
+  )
+data_merged <- data_merged %>%
+  inner_join(roll_vol_breakpoints, by = "year")
+
+# flag rolling volume
+data_merged <- data_merged %>%
+  mutate(roll_vol_type = case_when(roll_vol <= roll_vol_q20 ~ 'L',
+                                   roll_vol > roll_vol_q20 &
+                                     roll_vol < roll_vol_q80 ~ 'N',
+                                   roll_vol >= roll_vol_q80 ~ 'H')
+  )
+
+# compute the breakpoints of ILLIQ
+ILLIQ_breakpoints <- data_merged %>%
+  group_by(year) %>%
+  summarise(
+    ILLIQ_q20 = quantile(ILLIQ, 0.2, na.rm = TRUE),
+    ILLIQ_q80 = quantile(ILLIQ, 0.8, na.rm = TRUE)
+  )
+data_merged <- data_merged %>%
+  inner_join(ILLIQ_breakpoints, by = "year")
+
+# flag ILLIQ
+data_merged <- data_merged %>%
+  mutate(ILLIQ_type = case_when(ILLIQ <= ILLIQ_q20 ~ 'L',
+                                ILLIQ > ILLIQ_q20 & ILLIQ < ILLIQ_q80 ~ 'N',
+                                ILLIQ >= ILLIQ_q80 ~ 'H')
+  )
+
+# pick the needed "type" variables
+data_typed <- data_merged %>%
+  select(year,month, PERMNO,RET, size, BM_type, Size_type, roll_vol_type, ILLIQ_type) %>%
+  group_by(PERMNO) %>%
+  mutate(weight = lag(size))
