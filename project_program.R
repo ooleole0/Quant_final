e0d3d9e4aa986d0acd2c5fc21f7793d9b36833ea
ooleole0 <oo4766@gmail.com>
2022-09-11 11:07:45 +0800

    fix weighted mean with NA weights problems

--- project_program.R
+++ project_program.R
@@ -39,33 +39,37 @@ data <- data %>%
 # read vincent's fama-related data
 data_fama <- read_csv("chars_data.csv") %>%
   select(PERMNO, gvkey, sorting_year, BM, size)
+
 data_mktcap <- read_csv("mktcap_monthly.csv") %>%
   select(PERMNO, year, month, mktcap)
+
 # merge liquidity and fama-related variables
 data_merged <- data %>%
   inner_join(data_fama, by = c("PERMNO","sorting_year")) %>%
-  inner_join(data_mktcap, by = c("PERMNO","year", "month"))
+  inner_join(data_mktcap, by = c("PERMNO","year", "month")) %>%
+  drop_na()
+
 # remove unnecessary data to save memories
 rm(data, data_fama, data_mktcap)
 
 # compute the breakpoints of BM
 BM_breakpoints <- data_merged %>%
-  group_by(year) %>%
+  group_by(sorting_year) %>%
   summarise(
     BM_q30 = quantile(BM, 0.3, na.rm = TRUE),
     BM_q70 = quantile(BM, 0.7, na.rm = TRUE)
   )
 data_merged <- data_merged %>%
-  inner_join(BM_breakpoints, by = "year")
+  inner_join(BM_breakpoints, by = "sorting_year")
 
 # compute the breakpoints of size
 size_breakpoints <- data_merged %>% 
-  group_by(year) %>%
+  group_by(sorting_year) %>%
   summarise(
     size_median = quantile(size, 0.5, na.rm = TRUE),
   )
 data_merged <- data_merged %>%
-  inner_join(size_breakpoints,by = "year")
+  inner_join(size_breakpoints,by = "sorting_year")
 
 # flag BM and size
 data_merged <- data_merged %>%
@@ -78,13 +82,13 @@ data_merged <- data_merged %>%
 
 # compute the breakpoints of rolling volume
 roll_vol_breakpoints <- data_merged %>%
-  group_by(year) %>%
+  group_by(sorting_year) %>%
   summarise(
     roll_vol_q20 = quantile(roll_vol, 0.2, na.rm = TRUE),
     roll_vol_q80 = quantile(roll_vol, 0.8, na.rm = TRUE)
   )
 data_merged <- data_merged %>%
-  inner_join(roll_vol_breakpoints, by = "year")
+  inner_join(roll_vol_breakpoints, by = "sorting_year")
 
 # flag rolling volume
 data_merged <- data_merged %>%
@@ -96,13 +100,13 @@ data_merged <- data_merged %>%
 
 # compute the breakpoints of ILLIQ
 ILLIQ_breakpoints <- data_merged %>%
-  group_by(year) %>%
+  group_by(sorting_year) %>%
   summarise(
     ILLIQ_q20 = quantile(ILLIQ, 0.2, na.rm = TRUE),
     ILLIQ_q80 = quantile(ILLIQ, 0.8, na.rm = TRUE)
   )
 data_merged <- data_merged %>%
-  inner_join(ILLIQ_breakpoints, by = "year")
+  inner_join(ILLIQ_breakpoints, by = "sorting_year")
 
 # flag ILLIQ
 data_merged <- data_merged %>%
@@ -117,10 +121,20 @@ data_typed <- data_merged %>%
   group_by(PERMNO) %>%
   mutate(weight = lag(mktcap))
 
+# construct new weighted mean function since weighted.mean doesn't handle NA weights
+weighted_mean = function(x, w, ..., na.rm=F){
+  if(na.rm){
+    keep = !is.na(x)&!is.na(w)
+    w = w[keep]
+    x = x[keep]
+  }
+  weighted.mean(x, w, ..., na.rm=F)
+}
+
 # valuate fama and roll_vol return
 portf_3x2x3_vol <- data_typed %>% 
   group_by(date,BM_type,Size_type, roll_vol_type) %>% 
-  summarise(vwret = weighted.mean(100*RET,w=weight,na.rm=T))
+  summarise(vwret = weighted_mean(100 * RET, w = weight,na.rm = T))
 
 portf_3x2x3_vol <- portf_3x2x3_vol %>% 
   pivot_wider(
@@ -133,7 +147,7 @@ portf_3x2x3_vol <- portf_3x2x3_vol %>%
 # valuate fama and ILLIQ return
 portf_3x2x3_ILLIQ <- data_typed %>% 
   group_by(date,BM_type,Size_type, ILLIQ_type) %>% 
-  summarise(vwret = weighted.mean(100*RET,w=weight,na.rm=T))
+  summarise(vwret = weighted_mean(100 * RET, w = weight, na.rm = T))
 
 portf_3x2x3_ILLIQ <- portf_3x2x3_ILLIQ %>% 
   pivot_wider(
@@ -142,3 +156,17 @@ portf_3x2x3_ILLIQ <- portf_3x2x3_ILLIQ %>%
     names_from = c(BM_type,Size_type, ILLIQ_type),
     names_sep = ""
   )
+
+# valuate roll_vol return
+portf_3_vol <- data_typed %>% 
+  group_by(date,roll_vol_type) %>% 
+  summarise(vwret = weighted_mean(100 * RET,w = weight, na.rm = T))
+
+portf_3_vol <- portf_3_vol %>% 
+  pivot_wider(
+    id_cols = date,
+    values_from= vwret,
+    names_from = c(roll_vol_type),
+    names_sep = ""
+  )
+
